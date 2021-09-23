26630eec164b2cf2e10d0af88cfae978025a2f8b
xiezhang <xiezhang@redhat.com>
2021-09-23 16:31:43 -0400

    KOGITO-3759: Create an example for enabling the Outbox pattern with MongoDB and Debezium

--- process-outbox-mongodb-springboot/src/test/java/org/kie/kogito/examples/demo/OutboxIT.java
+++ process-outbox-mongodb-springboot/src/test/java/org/kie/kogito/examples/demo/OutboxIT.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kie.kogito.examples;
+package org.kie.kogito.examples.demo;
 
 import java.time.Duration;
 import java.util.HashMap;
